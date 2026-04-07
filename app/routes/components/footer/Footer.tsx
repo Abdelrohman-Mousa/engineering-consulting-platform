d4781a4b320bf86a02e13cb4f9d2e5af8e80834e
Abdelrohman-Mousa <abdelrohmanmarey@gmail.com>
2026-04-07 06:04:47 +0200

    Footer

--- app/routes/components/footer/Footer.tsx
+++ app/routes/components/footer/Footer.tsx
@@ -1,6 +1,113 @@
+import './footer.scss';
+import arrow from "/assets/icons/right-arrow.png";
+import phone from "/assets/icons/phone.svg";
+import email from "/assets/icons/email.svg";
+import location from "/assets/icons/location.svg";
+import face from "/assets/icons/facebook.svg";
+import insta from "/assets/icons/instagram.svg";
+import snap from "/assets/icons/snapchat.svg";
+import tiktok from "/assets/icons/tiktok.svg";
+import {ShinyButton} from "~/components/ui/shiny-button";
+import {Link} from "react-router";
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-logo">
+                    <div className="image">
+                        <img src="/assets/icons/logoProjects.png" alt="logo" />
+                    </div>
+                    <h2 className="pra">We provide innovative and precise engineering solutions.</h2>
+
+                  <ShinyButton style={{width: "fit-content"}}>
+                    <div className="btn">
+                        <h3>Get a free consulting</h3>
+                        <div className="arrow">
+                          <img src={arrow} alt="arrow" />
+                        </div>
+                    </div>
+                  </ShinyButton>
+                </div>
+
+                <div className="footer-quick-links">
+                    <h1 className="title">Quick Links</h1>
+                        <ul>
+                            <Link to="/">
+                               <li>Home</li>
+                            </Link>
+                            <Link to="/about">
+                                <li>About</li>
+                            </Link>
+                            <Link to="/consultationRequest">
+                                <li>Consultation Request</li>
+                            </Link>
+                            <Link to="/projects">
+                                <li>Projects</li>
+                            </Link>
+                            <Link to="/contactUs">
+                                <li>Contact</li>
+                            </Link>
+                        </ul>
+                </div>
+
+                <div className="footer-contact">
+                    <h1 className="title">Contact</h1>
+                    <div className="contact-info">
+                        <div className="info">
+                            <img src={phone} alt="phone" />
+                            <p>+ 050-37 0 27 59</p>
+                        </div>
+                        <div className="info">
+                            <img src={email} alt="email" />
+                            <p>advance.consultant@yahoo.com</p>
+                        </div>
+                        <div className="info">
+                            <img src={location} alt="phone" />
+                            <p>Abu Dhabi United Arab Emirates</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer-social">
+                    <h1 className="title">Social Media</h1>
+                    <div className="social-icons">
+                        <a href="#"
+                           target="_blank"
+                           rel="noreferrer"
+                           className="icon">
+                            <img src={face} alt="facebook" />
+                            <p>facebook</p>
+                        </a>
+
+                        <a href="#"
+                           target="_blank"
+                           rel="noreferrer"
+                           className="icon">
+                            <img src={insta} alt="facebook" />
+                            <p>Instagram</p>
+                        </a>
+
+                        <a href="#"
+                           target="_blank"
+                           rel="noreferrer"
+                           className="icon">
+                            <img src={snap} alt="facebook" />
+                            <p>Snapchat</p>
+                        </a>
+
+                        <a href="#"
+                           target="_blank"
+                           rel="noreferrer"
+                           className="icon">
+                            <img src={tiktok} alt="facebook" />
+                            <p>TikTok</p>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Footer
