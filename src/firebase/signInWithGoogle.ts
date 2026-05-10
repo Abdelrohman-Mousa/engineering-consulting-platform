217158c6d82136ab8592bbc0ebbc74dae562d104
Abdelrohman-Mousa <abdelrohmanmarey@gmail.com>
2026-05-10 17:50:22 +0300

    Role Admin Google

--- src/firebase/signInWithGoogle.ts
+++ src/firebase/signInWithGoogle.ts
@@ -1,29 +1,75 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+    doc,
+    getDoc,
+    serverTimestamp,
+    setDoc
+} from "firebase/firestore";
+
 import { auth, db } from "./firebaseConfig";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+
+import {
+    signInWithPopup,
+    GoogleAuthProvider
+} from "@firebase/auth";
 
 const provider = new GoogleAuthProvider();
 
 export const loginWithGoogle = async () => {
+
     try {
-        const result = await signInWithPopup(auth, provider);
+
+        const result =
+            await signInWithPopup(auth, provider);
+
         const user = result.user;
 
-        await setDoc(
-            doc(db, "users", user.uid),
-            {
+        const userRef =
+            doc(db, "users", user.uid);
+
+        const userSnap =
+            await getDoc(userRef);
+
+        // لو المستخدم جديد فقط
+        if (!userSnap.exists()) {
+
+            await setDoc(userRef, {
+
                 email: user.email,
-                name: user.displayName || user.email.split("@")[0],
-                imageUrl: user.photoURL,
+
+                name:
+                    user.displayName ||
+                    user.email?.split("@")[0] ||
+                    "User",
+
+                imageUrl:
+                    user.photoURL ||
+                    `https://ui-avatars.com/api/?name=${encodeURIComponent(
+                        user.displayName || "User"
+                    )}`,
+
                 role: "user",
-                createdAt: serverTimestamp(),   // لو جديد
-                lastLogin: serverTimestamp(),   // 👈 أهم حاجة
-            },
-            { merge: true }
-        );
+
+                createdAt: serverTimestamp(),
+
+                lastLogin: serverTimestamp(),
+            });
+
+        } else {
+
+            // تحديث آخر تسجيل دخول فقط
+            await setDoc(
+                userRef,
+                {
+                    lastLogin: serverTimestamp(),
+                },
+                { merge: true }
+            );
+        }
 
         return user;
+
     } catch (err) {
+
         throw err;
     }
 };
